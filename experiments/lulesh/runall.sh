c5adc419f0c548f4f3ad2c4674e1fa5772174889
jan_hu <jhueckelheim@anl.gov>
2020-10-09 21:04:39 +0000

    removed map and btree implementation from lulesh test case, they take too much time to be useful

--- experiments/lulesh/runall.sh
+++ experiments/lulesh/runall.sh
@@ -3,12 +3,12 @@ module use /soft/modulefiles
 module load intel/2019
 
 make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=LOCK 
-make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=BTREE 
+#make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=BTREE 
 make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=BLOCK 
 make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=ATOMIC 
 make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=KEEPER 
 make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=DENSE 
-make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=MAP 
+#make -f Makefile_spray --always-make CC=icc REDUCTIONSTYLE=MAP 
 make -f Makefile CC=icc
 
 set -x
@@ -22,8 +22,8 @@ do
 		./lulesh2.0        -s $ns
 		echo "LOCK"
 		./lulesh2.0_LOCK   -s $ns
-		echo "BTREE"
-		./lulesh2.0_BTREE  -s $ns
+#		echo "BTREE"
+#		./lulesh2.0_BTREE  -s $ns
 		echo "BLOCK"
 		./lulesh2.0_BLOCK  -s $ns
 		echo "ATOMIC"
@@ -32,7 +32,7 @@ do
 		./lulesh2.0_KEEPER -s $ns
 		echo "DENSE"
 		./lulesh2.0_DENSE  -s $ns
-		echo "MAP"
-		./lulesh2.0_MAP    -s $ns
+#		echo "MAP"
+#		./lulesh2.0_MAP    -s $ns
 	done
 done
