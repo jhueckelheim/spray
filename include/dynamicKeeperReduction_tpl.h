8f0c297cd572c49f7d4ededda94f1692d0f6162c
Johannes Doerfert <johannes@jdoerfert.de>
2021-05-10 18:51:43 -0500

    Play around with prefetching and delayed atomics

--- include/dynamicKeeperReduction_tpl.h
+++ include/dynamicKeeperReduction_tpl.h
@@ -7,14 +7,21 @@
 
 extern int Hits, Misses;
 
+#define likely(x) __builtin_expect(!!(x),1)
+#define unlikely(x) __builtin_expect(!!(x),0)
+
 typedef struct {
   T *content;
   Bitarray atomic_array;
+  int idx;
+  T val;
 } TEMPLATE(spray_keeper,T);
 
 void TEMPLATE(spray_keeper_init,T)(TEMPLATE(spray_keeper,T)* init, T *orig,  Bitarray atomic_array) {
   init->content = orig;
   init->atomic_array = atomic_array;
+  init->idx = 0;
+  init->val = 0;
 }
 
 void TEMPLATE(_spray_keeper_ompinit,T)(TEMPLATE(spray_keeper,T) *__restrict__ init,
@@ -23,13 +30,20 @@ void TEMPLATE(_spray_keeper_ompinit,T)(TEMPLATE(spray_keeper,T) *__restrict__ in
 }
 
 void TEMPLATE(spray_keeper_finalize,T)(TEMPLATE(spray_keeper,T) *__restrict__ obj) {
+  #pragma omp atomic update
+  obj->content[obj->idx] += obj->val;
 }
 
 void TEMPLATE(spray_keeper_increment,T)(TEMPLATE(spray_keeper,T) *obj, int x, int idx, T val) {
+  __builtin_prefetch(&obj->content[obj->idx], 1, 3);
   __builtin_prefetch(&obj->content[idx], 1, 3);
   if (bitarray_get(obj->atomic_array, x)) {
-    #pragma omp atomic update
-    obj->content[idx] += val;
+    if (likely(obj->val)) {
+      #pragma omp atomic update
+      obj->content[obj->idx] += obj->val;
+    }
+    obj->idx = idx;
+    obj->val = val;
     //#pragma omp atomic
     //++Misses;
   } else {
